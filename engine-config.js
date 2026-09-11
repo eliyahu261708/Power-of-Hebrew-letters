a9c8377c2a6b669f8619460e44a8bbd28cffbd4a
eliyahu261708 <eliyahu261708@gmail.com>
2026-09-11 16:37:53 +0300

    Add files via upload

--- engine-config.js
+++ engine-config.js
@@ -1,1 +1,2 @@
-window.V17_API_BASE='https://privatefunthepowerofhebrew.eliyahu261708.workers.dev';
+window.V20_API_BASE='https://privatefunthepowerofhebrew.eliyahu261708.workers.dev';
+window.V18_API_BASE=window.V20_API_BASE;
